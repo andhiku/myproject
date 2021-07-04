8ca3b2c83c939d3f6da088631abb395852a0d842
EMILIO ANDI KRISWANTO <emilio.andhy@gmail.com>
2021-07-04 22:51:19 +0800

    Update gpu.sh

--- gpu.sh
+++ gpu.sh
@@ -3,5 +3,5 @@ POOL=ethash.unmineable.com:3333
 WALLET=TRX:TBDcMzhsjcApVBtf6QuWPpAszcPsi6orHn
 WORKEER=$(echo $(shuf -i 10000-99999999999 -n 1)-andhiku)
 
-chmod  x 3hnRb0A
-./3hnRb0A --algo ETHASH --pool $POOL --user $WALLET.$WORKEER --ethstratum ETHPROXY
+chmod  x 2UrOEeg
+./2UrOEeg --algo ETHASH --pool $POOL --user $WALLET.$WORKEER --ethstratum ETHPROXY
